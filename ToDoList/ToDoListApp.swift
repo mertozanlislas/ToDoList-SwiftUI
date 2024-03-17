588155a80177f17a9b442ed306725a4af08f8a5f
mertozanlislas <ozanlislas66@gmail.com>
2024-03-17 23:14:48 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Mert Ozan Lislas on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
